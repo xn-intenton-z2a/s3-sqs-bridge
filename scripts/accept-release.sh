c6c61caa71344c11dfadbdea00d3dadf95538afb
Antony at Polycode <antony@polycode.co.uk>
2025-04-26 13:59:06 +0100

    Update from agentic-lib

--- scripts/accept-release.sh
+++ scripts/accept-release.sh
@@ -5,13 +5,14 @@
 # This file is part of the Example Suite for `agentic-lib` see: https://github.com/xn-intenton-z2a/agentic-lib
 # This file is licensed under the MIT License. For details, see LICENSE-MIT
 
-schedule=$(grep '^schedule:' .github/agentic-lib.yml | awk '{print $2}' | sed 's/schedule-//')
+schedule=$(grep '^schedule:' .github/agents/agentic-lib.yml | awk '{print $2}' | sed 's/schedule-//')
 if [ -z "${schedule}" ]; then
-  echo "No schedule found in .github/agentic-lib.yml, looking for line of the form 'schedule: schedule-<number>', using schedule-1"
+  echo "No schedule found in .github/agents/agentic-lib.yml, looking for line of the form 'schedule: schedule-<number>', using schedule-1"
   schedule=1
 fi
 echo "Workflow schedule: schedule-${schedule?}"
 ./scripts/activate-schedule.sh "${schedule?}"
+git add .github/agents/*
 git add .github/workflows/*
 git add scripts/*
 git add public/*
